b183971e2e749d5647faf1e5589d1ff3de2a98c9
yeo99 <qw6375@gmail.com>
2023-12-14 15:24:07 +0900

    [ADD] 웹소켓 추가

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http"
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -10,5 +12,13 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
 app.get("*", (req, res) => res.redirect("/")); // 다른 url 사용 안할 예정
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`) // 프로토콜에 ws를 입력할 수도 있다.
+
+const server = http.createServer(app);
+
+/**
+ * 이렇게 하면 http 서버, webSocket서버 둘 다 돌릴 수 있다.
+ * 이러면 동일한 포트에서 http, 웹소켓 요청 두개를 다 처리할 수 있다.
+ */
+const wss = new WebSocket.Server({server})
+server.listen(3000, handleListen)
